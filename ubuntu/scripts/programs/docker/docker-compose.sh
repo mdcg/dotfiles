84ffbb58bef5bb3f29cbb1b465eb495a29c6c112
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-04-09 20:56:17 -0300

    feat: adding more info in installation scripts

--- ubuntu/scripts/programs/docker/docker-compose.sh
+++ ubuntu/scripts/programs/docker/docker-compose.sh
@@ -3,6 +3,7 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install docker-compose -y
 
+# Reference: https://docs.docker.com/compose/install/
 # You can also install this way:
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo chmod +x /usr/local/bin/docker-compose
--- ubuntu/scripts/programs/pyenv/pyenv.sh
+++ ubuntu/scripts/programs/pyenv/pyenv.sh
@@ -3,4 +3,6 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+echo "Copying config to the ~/.bashrc file"
 cat config >> ~/.bashrc
+echo "Run the command 'source ~/.bashrc' for the modifications to take effect."
