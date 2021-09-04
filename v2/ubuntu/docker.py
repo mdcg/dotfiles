53cbf16463843e44719c914675877c67b0f95f81
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-09-04 12:45:06 -0300

    apply PEP8

--- v2/ubuntu/docker.py
+++ v2/ubuntu/docker.py
@@ -50,7 +50,7 @@ def install_docker():
         "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"
     )
     exec_command(
-        "echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
+        'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
     )
 
     exec_update()
