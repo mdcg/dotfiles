ec02eff194e80d0d32110bf5eac657a426b6dd7a
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-09-07 18:08:18 -0300

    creating github install script

--- v2/ubuntu/ohmyzsh.py
+++ v2/ubuntu/ohmyzsh.py
@@ -10,9 +10,15 @@ def install_ohmyzsh():
     exec_update()
     exec_upgrade()
 
-    exec_command(command="sudo apt install zsh", label=f"🪄 {Fore.GREEN}{Style.BRIGHT} Installing ZSH...")
+    exec_command(
+        command="sudo apt install zsh",
+        label=f"🪄 {Fore.GREEN}{Style.BRIGHT} Installing ZSH...",
+    )
 
     exec_command("chsh -s $(which zsh)")
 
     exec_command("chmod +x install.sh")
-    exec_command(command="./install.sh", label=f"🪄 {Fore.GREEN}{Style.BRIGHT} Installing OhMyZsh...")
+    exec_command(
+        command="./install.sh",
+        label=f"🪄 {Fore.GREEN}{Style.BRIGHT} Installing OhMyZsh...",
+    )
