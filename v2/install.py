edcf0de5d577767e600e0ca8c012a4ce10594fd1
Mauro de Carvalho <mauro.dcg@gmail.com>
2021-09-07 14:48:01 -0300

    creating ohmyzsh installation script

--- v2/install.py
+++ v2/install.py
@@ -2,8 +2,11 @@ from v2.ubuntu.docker import install_docker
 from v2.ubuntu.generics import install_generic_utils
 from v2.ubuntu.vscode import install_vscode
 from v2.ubuntu.brave import install_brave
+from v2.ubuntu.ohmyzsh import install_ohmyzsh
+
 
 install_generic_utils()
+install_ohmyzsh()
 install_docker()
 install_vscode()
 install_brave()
---
+++ v2/ubuntu/ohmyzsh.py
@@ -0,0 +1,18 @@
+from v2.ubuntu import (
+    exec_update,
+    exec_upgrade,
+    exec_command,
+)
+from colorama import Fore, Style
+
+
+def install_ohmyzsh():
+    exec_update()
+    exec_upgrade()
+
+    exec_command(command="sudo apt install zsh", label=f"🪄 {Fore.GREEN}{Style.BRIGHT} Installing ZSH...")
+
+    exec_command("chsh -s $(which zsh)")
+
+    exec_command("chmod +x install.sh")
+    exec_command(command="./install.sh", label=f"🪄 {Fore.GREEN}{Style.BRIGHT} Installing OhMyZsh...")
