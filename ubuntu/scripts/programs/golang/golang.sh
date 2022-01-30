b8a5a175b8c0e9732dd9bf7437d805a9f039791f
Mauro de Carvalho <mauro.dcg@gmail.com>
2022-01-30 14:35:30 -0300

    feat: handling with golang in zsh

--- ubuntu/scripts/programs/golang/golang.sh
+++ ubuntu/scripts/programs/golang/golang.sh
@@ -3,4 +3,5 @@
 wget -L https://golang.org/dl/go1.17.6.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
 cat golang >> ~/.bashrc
+cat golang >> ~/.zshrc
 rm go1.17.6.linux-amd64.tar.gz
