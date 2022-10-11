f5bbb975d3d526ac96bc989f58d388a5be3304e1
Mauro de Carvalho <mauro.dcg@gmail.com>
2022-10-11 17:48:05 -0300

    feat: updating ohmyzsh and pyenv installation scripts

--- ubuntu/ohmyzsh/ohmyzsh.sh
+++ ubuntu/ohmyzsh/ohmyzsh.sh
@@ -2,7 +2,7 @@
 
 sudo apt install zsh
 chsh -s $(which zsh)
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Now run `sh install.sh`"
 echo "If it can't works, reboot the machine."
 echo ">>> Remember: we like to use 'af-magic' theme <<<"
--- ubuntu/scripts/programs/pyenv/pyenv.sh
+++ ubuntu/scripts/programs/pyenv/pyenv.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
-curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 echo "Copying config to the ~/.bashrc and ~/.zshrc files"
 cat config >> ~/.bashrc
 cat config >> ~/.zshrc
